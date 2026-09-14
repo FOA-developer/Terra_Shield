a1b26a2abb42c55ce23cb2e73a7ff837305b5d62
FOA-developer <flourishagbanoma@gmail.com>
2026-09-14 08:05:11 +0100

    feat: wrap DataProvider with AuthProvider for improved context management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { DataProvider } from './data/DataContext'
+import { AuthProvider } from './auth/AuthContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <DataProvider>
-        <App />
-      </DataProvider>
+      <AuthProvider>
+        <DataProvider>
+          <App />
+        </DataProvider>
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 )
